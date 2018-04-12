d2f2fbe918a2415b55de5371878d4793c94fcfbb
Pedro Crispim <pedro@loopsoftware.fr>
2018-04-12 17:58:17 +0100

    fixed some issues

--- invoicing/1.2.3/µServices/test_service/service.js
+++ invoicing/1.2.3/µServices/test_service/service.js
@@ -4,12 +4,22 @@ const {YNotificationData} = require("./framework/server/lib/notifications/YNotif
 const {YEvent} = require("./framework/server/lib/notifications/YEvent.js");
 
 exports.oninitServiceFabric = function(serv) {
-    serv.service.registerTask("test emit event", 60, (server, role, session) => {
+    console.log(">>> oninitServiceFabric >>> about to register task...");
+
+    serv.service.registerTask("TestEmitEvent", 60, (server, role, session) => {
+        console.log(">>> oninitServiceFabric > task callback >>> about to call emit event...");
+
         return emitEvent(server, role, session);
     }, 120);
+
+    console.log(">>> oninitServiceFabric >>> done registering task.");
+
+    return Promise.resolve();
 }
 
 function emitEvent(server, role, session) {
+    console.log(">>> emitEvent >>> about to emit event...");
+
     const data = YNotificationData.fromJSON({
         subject: "µService Notification",
         body: "This is an example notification for an event emitted from a micro-service.",
@@ -22,16 +32,24 @@ function emitEvent(server, role, session) {
         args: {}
     });
 
-    const event = new YEvent({userIds: ["eace59be-792e-4aad-8dfb-43f4e12bafee"]}, session, Date.now().toISOString(), data, null, null);
+    console.log(`>>> emitEvent >>> created notification data object... ${data.toJSON()}`);
+
+    const event = new YEvent({userIds: ["eace59be-792e-4aad-8dfb-43f4e12bafee"]}, session, new Date().toISOString(), data, null, null);
+
+    console.log(`>>> emitEvent >>> created event object... ${event.toJSON()}`);
+
+    console.log(">>> emitEvent >>> emitting event...");
 
     return event.emit()
         .then(result => {
-            console.log(JSON.stringify(result));
+            console.log(">>> emitEvent > emit().then() >>> emitted event...");
+
+            console.log(`>>> emitEvent > emit().then() resolve >>> result: ${JSON.stringify(result)}`);
         }, error => {
-            console.warning(`Something not good happened: ${error.message}`);
+            console.log(`>>> emitEvent > emit().then() reject >>> something not good happened: ${error.message}`);
         })
         .catch(error => {
-            console.error(`Something bad happened: ${error.message}`)
+            console.log(`>>> emitEvent > emit().then() catch >>> something bad happened: ${error.message}`);
 
             error.critical = false;
             throw error;
