0fae2ca7469735d525e3263ca2d57f4520e66079
Pedro Crispim <pedro@loopsoftware.fr>
2018-05-03 16:14:46 +0100

    replaced deprecated usage of callback with promise call pf YDBServer.connect()

--- invoicing/1.2.3/functions/server/total.js
+++ invoicing/1.2.3/functions/server/total.js
@@ -1,13 +1,11 @@
+"use strict";
+
 const yQuery = require('./YQuery.js').YQuery;
 
 exports.REST = {
     total: (_args, _content, _serv, _role, _session) => {
-        return new Promise((_resolve, _reject) => {
-            _session.dbserver.connect("nsC", 'INVOICING', function (_err, _cnx) {
-                if (_err) {
-                    _reject(_err);
-                }
-
+        return _session.dbserver.connect("nsC", 'INVOICING')
+            .then(function (_cnx) {
                 const query = yQuery.From('invoicing.invoice.lines')
                     .Include('obj.product')
                     .Where('obj.parentObjectId == "' + _args.invoiceID + '"')
@@ -15,12 +13,13 @@ exports.REST = {
                     .Sum('obj.amount * obj.product.price.amount')
                     .Execute();
 
-                _cnx.find(query).then(_yr => {
-                    _resolve({
-                        data: _yr.data[0]
+                return _cnx.find(query)
+                    .then(function (_yr) {
+                        return {data: _yr.data[0]};
                     });
-                }).catch(_reject);
+            })
+            .catch(error => {
+                return Promise.reject(error);
             });
-        });
     }
 };
