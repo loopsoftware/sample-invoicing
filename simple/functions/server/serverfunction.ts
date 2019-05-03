e00d243494f051f51cc5c613517fff32b2f19959
AlexYup <alex@loopsoftware.fr>
2019-05-03 17:54:49 +0100

    typescript querying data

--- simple/functions/server/serverfunction.ts
+++ simple/functions/server/serverfunction.ts
@@ -3,6 +3,8 @@
 
 import {YRole} from "@yupana/framework-types/lib/YRoles";
 import {YSession} from "@yupana/framework-types/lib/YSessions";
+import {YQuery} from "@yupana/framework-types/";
+import {Model} from "../../ts-classes/model";
 
 type ServerFunctionResponse<T> = Promise<{data:T}>
 
@@ -37,5 +39,45 @@ exports.REST = {
         phrase: string
     }> => {
         return {data: {success: true, phrase: _args.phrase}};
+    },
+
+    $query: {
+        module: "simple",
+        name: "query",
+        scope: "public",
+        description: "returns calculated property of a model from the DB",
+        restricted: false,
+        args: [
+            {
+                name: "title",
+                type: "string",
+                mandatory: true,
+                description: "title of the model to search by"
+            }
+        ],
+        content: [],
+        output: "{success: true, content: 'qwe(asd)'}",
+        example: "/simple/serverfunction/query?title=qwe  =>  {success: true, content: 'qwe (asd)'}",
+        method: "get"
+    },
+    query: async (
+        _args: { title: string },
+        _content: any,
+        _serv: any,
+        _role: YRole,
+        _session: YSession
+    ): ServerFunctionResponse<{
+        success: boolean,
+        content: string
+    }> => {
+        const cnx = await _session.dbserver.connect("simple", 'simple', null);
+        const query = YQuery.From('simple.model')
+                    .Where(`obj.title == "${_args.title}"`)
+                    .Select()
+                    .Execute();
+        const result = await cnx.find(query);
+        const firstMatch:Model = _role.simple.createClass("Model");
+        firstMatch.setData(await result.iterator.next(null));
+        return {data: {success: true, content: firstMatch.cGetContent()}};
     }
 };
