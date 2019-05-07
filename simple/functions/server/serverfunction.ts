1946f70457771d96d2dd97fb8c48e85c16a0be19
AlexYup <alex@loopsoftware.fr>
2019-05-07 18:16:37 +0100

    typescript inserting data

--- simple/functions/server/serverfunction.ts
+++ simple/functions/server/serverfunction.ts
@@ -5,6 +5,7 @@ import {YRole} from "@yupana/framework-types/lib/YRoles";
 import {YSession} from "@yupana/framework-types/lib/YSessions";
 import {YQuery} from "@yupana/framework-types/";
 import {Model} from "../../ts-classes/model";
+import {YTstring} from "@yupana/class-generator";
 
 type ServerFunctionResponse<T> = Promise<{data:T}>
 
@@ -76,8 +77,60 @@ exports.REST = {
                     .Select()
                     .Execute();
         const result = await cnx.find(query);
-        const firstMatch:Model = _role.simple.createClass("Model");
+        const firstMatch:Model = await _role.simple.createClass("model");
         firstMatch.setData(await result.iterator.next(null));
         return {data: {success: true, content: firstMatch.cGetContent()}};
+    },
+
+    $add: {
+        module: "simple",
+        name: "add",
+        scope: "public",
+        description: "add a new model with given title and description",
+        restricted: false,
+        args: [],
+        content: [
+            {
+                name: "title",
+                type: "string",
+                mandatory: true,
+                description: "title of the model"
+            },
+            {
+                name: "description",
+                type: "string",
+                mandatory: true,
+                description: "description of the model"
+            }
+
+        ],
+        output: "{success: true}",
+        example: "/simple/serverfunction/add",
+        method: "post"
+    },
+    add: async (
+        _args: any,
+        _content: { title: string, description: string },
+        _serv: any,
+        _role: YRole,
+        _session: YSession
+    ): ServerFunctionResponse<{
+        success: boolean
+    }> => {
+        const newModel:Model = await _role.simple.createClass("model");
+        // .setData doesn't work
+        // newModel.setData(_content);
+        // this does:
+        newModel.title = _content.title as YTstring;
+        newModel.description = _content.description as YTstring;
+        newModel.validate(err => {
+            if (err) {
+                throw err;
+            }
+        }, false);
+
+        const cnx = await _session.dbserver.connect("simple", 'simple', null);
+        await cnx.commit([newModel]);
+        return {data: {success: true}};
     }
 };
