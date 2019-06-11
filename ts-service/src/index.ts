eff213e5b5e63b688e7daa2ad0bf2dab8944bbc2
AlexYup <alex@loopsoftware.fr>
2019-06-11 17:57:19 +0100

    updated yupana types library, added readme for service, styled up

--- ts-service/src/index.ts
+++ ts-service/src/index.ts
@@ -1,21 +1,21 @@
-import {Service} from "@yupana/framework-service-types";
 import {YContext} from "@yupana/framework-types/lib/YContext";
 import {YRole} from "@yupana/framework-types/lib/YRoles";
 import {YSession} from "@yupana/framework-types/lib/YSessions";
 import {YLog, YQuery} from "@yupana/framework-types";
 import {Model} from "../../simple/ts-classes/model";
+import {Service} from "@yupana/framework-types/lib/Server";
 
 async function worker(serv: YContext<Service>, role: YRole, session: YSession) {
     console.log(">>> worker >>> about to fetch data ...");
 
-    const cnx = await session.dbserver.connect("simple", 'simple');
-    const query = YQuery.From('simple.model')
-                .Select()
-                .Execute();
+    const cnx = await session.dbserver.connect("simple", "simple");
+    const query = YQuery.From("simple.model")
+        .Select()
+        .Execute();
     const result = await cnx.find(query);
-    const m:Model = await role.simple.createClass("model");
+    const m: Model = await role.simple.createClass("model");
     let data = await result.iterator.next();
-    while(data) {
+    while (Boolean(data)) {
         m.setData(data);
         YLog.info(m.cGetContent());
         data = await result.iterator.next();
